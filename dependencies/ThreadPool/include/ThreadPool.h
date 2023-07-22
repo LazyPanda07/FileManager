31d68abcd131be332a62554d4618c200ae1f2953
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2023-07-22 12:37:57 +0500

    Update thread pool

--- dependencies/ThreadPool/include/ThreadPool.h
+++ dependencies/ThreadPool/include/ThreadPool.h
@@ -2,20 +2,12 @@
 
 #include <thread>
 #include <condition_variable>
-#include <queue>
 #include <mutex>
 #include <vector>
-#include <memory>
 #include <functional>
-#include <future>
 
-#ifdef THREAD_POOL_DLL
-#define THREAD_POOL_API __declspec(dllexport)
-
-#pragma warning(disable: 4251)
-#else
-#define THREAD_POOL_API
-#endif // THREAD_POOL_DLL
+#include "Tasks/FunctionWrapperTask.h"
+#include "Utility/ConcurrentQueue.h"
 
 namespace threading
 {
@@ -30,63 +22,79 @@ namespace threading
 		};
 
 	private:
-		struct threadPoolTask
+		struct Worker
 		{
-			std::function<void()> task;
-			std::function<void()> callback;
-			std::promise<void> notify;
+		private:
+			std::thread thread;
 
 		public:
-			threadPoolTask() = default;
+			mutable std::mutex stateMutex;
+			std::unique_ptr<BaseTask> task;
+			threadState state;
+			std::atomic_bool running;
+			std::function<void(Worker*)> onEnd;
 
-			threadPoolTask(const threadPoolTask& other) = delete;
+		public:
+			Worker(ThreadPool* threadPool);
 
-			threadPoolTask(threadPoolTask&& other) noexcept;
+			Worker(const Worker& other);
 
-			threadPoolTask& operator = (const threadPoolTask& other) = delete;
+			Worker& operator = (const Worker& other);
 
-			threadPoolTask& operator = (threadPoolTask&& other) noexcept;
+			void join();
 
-			~threadPoolTask() = default;
+			void detach();
+
+			~Worker() = default;
 		};
 
 	private:
-		std::queue<threadPoolTask> tasks;
+		utility::ConcurrentQueue<std::unique_ptr<BaseTask>> tasks;
 		std::condition_variable hasTask;
-		std::mutex tasksMutex;
-		std::vector<std::jthread> threads;
-		std::vector<threadState> threadStates;
-		bool terminate;
+		std::mutex workerMutex;
+		std::vector<Worker*> workers;
 
 	private:
-		void workerThread(size_t threadIndex);
+		void workerThread(Worker* worker);
 
-		std::future<void> addTask(threadPoolTask&& task);
+		std::unique_ptr<Future> addTask(std::unique_ptr<BaseTask>&& task);
 
 	public:
 		/// @brief Construct ThreadPool
 		/// @param threadCount Number of threads in ThreadPool(default is max threads for current hardware)
-		ThreadPool(uint32_t threadsCount = std::thread::hardware_concurrency());
+		ThreadPool(size_t threadsCount = std::thread::hardware_concurrency());
 
 		/// @brief Add new task to thread pool
-		std::future<void> addTask(const std::function<void()>& task, const std::function<void()>& callback = nullptr);
+		std::unique_ptr<Future> addTask(const std::function<void()>& task, const std::function<void()>& callback = nullptr);
 
 		/// @brief Add new task to thread pool
-		std::future<void> addTask(const std::function<void()>& task, std::function<void()>&& callback);
+		std::unique_ptr<Future> addTask(const std::function<void()>& task, std::function<void()>&& callback);
 
 		/// @brief Add new task to thread pool
-		std::future<void> addTask(std::function<void()>&& task, const std::function<void()>& callback = nullptr);
+		std::unique_ptr<Future> addTask(std::function<void()>&& task, const std::function<void()>& callback = nullptr);
 
 		/// @brief Add new task to thread pool
-		std::future<void> addTask(std::function<void()>&& task, std::function<void()>&& callback);
+		std::unique_ptr<Future> addTask(std::function<void()>&& task, std::function<void()>&& callback);
+
+		/// @brief Add new task to thread pool
+		template<typename R, typename... ArgsT, typename... Args>
+		std::unique_ptr<Future> addTask(const std::function<R(ArgsT...)>& task, const std::function<void()>& callback, Args&&... args);
+
+		/// @brief Add new task to thread pool
+		template<typename R, typename... ArgsT, typename... Args>
+		std::unique_ptr<Future> addTask(const std::function<R(ArgsT...)>& task, std::function<void()>&& callback, Args&&... args);
 
 		/// @brief Reinitialize thread pool
-		/// @param changeThreadsCount Changes current thread pool size
+		/// @param Wait all threads execution
 		/// @param threadsCount New thread pool size
-		void reinit(bool changeThreadsCount = false, uint32_t threadsCount = std::thread::hardware_concurrency());
+		void reinit(bool wait = true, size_t threadsCount = std::thread::hardware_concurrency());
 
-		/// @brief Stop ThreadPool with joining all threads
-		void shutdown();
+		/// @brief Change thread pool size
+		bool resize(size_t threadsCount);
+
+		/// @brief Stop ThreadPool
+		/// @param wait Wait all threads execution
+		void shutdown(bool wait = true);
 
 		/// @brief Check is thread pool has task that running in some thread
 		/// @return Returns true if thread pool has task
@@ -95,12 +103,37 @@ namespace threading
 		/// @brief Check specific thread state
 		/// @param threadIndex Index of thread between 0 and threadsCount
 		/// @return Thread state
-		threadState getThreadState(uint32_t threadIndex) const;
+		/// @exception std::out_of_range
+		threadState getThreadState(size_t threadIndex) const;
+
+		/// @brief Check specific thread progress
+		/// @param threadIndex Index of thread between 0 and threadsCount
+		/// @return Thread progress or -1 if thread not running any task
+		/// @exception std::out_of_range
+		float getThreadProgress(size_t threadIndex) const;
 
 		/// @brief Getter for threadsCount
 		/// @return Current count of threads in thread pool
-		uint32_t getThreadsCount() const;
+		size_t getThreadsCount() const;
 
 		~ThreadPool();
 	};
+
+	template<typename R, typename... ArgsT, typename... Args>
+	std::unique_ptr<Future> ThreadPool::addTask(const std::function<R(ArgsT...)>& task, const std::function<void()>& callback, Args&&... args)
+	{
+		return this->addTask
+		(
+			std::make_unique<FunctionWrapperTask<R>>(std::bind(task, std::forward<Args>(args)...), callback)
+		);
+	}
+
+	template<typename R, typename... ArgsT, typename... Args>
+	std::unique_ptr<Future> ThreadPool::addTask(const std::function<R(ArgsT...)>& task, std::function<void()>&& callback, Args&&... args)
+	{
+		return this->addTask
+		(
+			std::make_unique<FunctionWrapperTask<R>>(std::bind(task, std::forward<Args>(args)...), std::move(callback))
+		);
+	}
 }
