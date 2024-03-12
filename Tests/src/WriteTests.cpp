17f042c131db853dc97ee580861a65f7c79beabe
Semyon Gritsenko <40473939+LazyPanda07@users.noreply.github.com>
2024-03-12 16:15:26 +0500

    Add test

--- Tests/src/WriteTests.cpp
+++ Tests/src/WriteTests.cpp
@@ -7,6 +7,7 @@ using namespace std::chrono_literals;
 inline constexpr int threadsCount = 8;
 inline constexpr int writes = 512;
 inline constexpr int totalWrites = threadsCount * writes;
+inline constexpr size_t totalFiles = 128;
 
 void threadWrite(const std::string& fileName, const std::string& text)
 {
@@ -88,4 +89,66 @@ TEST(FileManager, Write)
 	}
 
 	ASSERT_EQ(count, totalWrites);
+}
+
+TEST(FileManager, MultipleFilesWrite)
+{
+	file_manager::FileManager& manager = file_manager::FileManager::getInstance();
+	std::vector<std::string> files;
+	std::vector<std::future<void>> threads;
+	vector<std::string> data;
+
+	threads.reserve(threadsCount);
+	
+	for (size_t i = 0; i < totalFiles; i++)
+	{
+		data.emplace_back();
+
+		const std::string& fileName = files.emplace_back(std::to_string(i) + ".txt");
+
+		std::ofstream file(fileName);
+
+		manager.addFile(fileName);
+	}
+
+	for (size_t i = 0; i < threadsCount; i++)
+	{
+		for (const std::string& fileName : files)
+		{
+			threads.emplace_back(std::async(std::launch::async, &threadWrite, std::ref(fileName), ""));
+		}
+	}
+
+	std::this_thread::sleep_for(1s);
+
+	for (std::future<void>& thread : threads)
+	{
+		thread.wait();
+	}
+
+	for (size_t index = 0; const std::string& fileName : files)
+	{
+		manager.readFile
+		(
+			fileName,
+			[&data, index](std::unique_ptr<file_manager::ReadFileHandle>&& handle)
+			{
+				data[index] = handle->readAllData();
+			}
+		);
+	}
+
+	for (const std::string& fileData : data)
+	{
+		std::istringstream is(fileData);
+		std::string line;
+		size_t count = 0;
+
+		while (std::getline(is, line))
+		{
+			count++;
+		}
+
+		ASSERT_EQ(count, totalWrites);
+	}
 }
